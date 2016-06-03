057ef9afcad59201852ac5825117f135d7945eff
Roman Nagaev <azwartn@gmail.com>
2016-06-03 10:28:35 +0300

    add qr code reader

---
+++ QRCodeApplication/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace QRCodeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.textQr = new System.Windows.Forms.TextBox();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnSaveToFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbBoxDevices = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(645, 270);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(99, 38);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Создать QR";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Считать QR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // image
+            // 
+            this.image.BackColor = System.Drawing.Color.White;
+            this.image.Location = new System.Drawing.Point(12, 13);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(400, 250);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image.TabIndex = 3;
+            this.image.TabStop = false;
+            // 
+            // textQr
+            // 
+            this.textQr.Location = new System.Drawing.Point(438, 13);
+            this.textQr.Multiline = true;
+            this.textQr.Name = "textQr";
+            this.textQr.Size = new System.Drawing.Size(306, 250);
+            this.textQr.TabIndex = 4;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 359);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(98, 42);
+            this.btnLoadFile.TabIndex = 5;
+            this.btnLoadFile.Text = "Из файла";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnSaveToFile
+            // 
+            this.btnSaveToFile.Location = new System.Drawing.Point(142, 359);
+            this.btnSaveToFile.Name = "btnSaveToFile";
+            this.btnSaveToFile.Size = new System.Drawing.Size(99, 42);
+            this.btnSaveToFile.TabIndex = 6;
+            this.btnSaveToFile.Text = "Сохранить в файл";
+            this.btnSaveToFile.UseVisualStyleBackColor = true;
+            this.btnSaveToFile.Click += new System.EventHandler(this.btnSaveToFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Запуcтить камеру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbBoxDevices
+            // 
+            this.cmbBoxDevices.FormattingEnabled = true;
+            this.cmbBoxDevices.Location = new System.Drawing.Point(13, 270);
+            this.cmbBoxDevices.Name = "cmbBoxDevices";
+            this.cmbBoxDevices.Size = new System.Drawing.Size(399, 21);
+            this.cmbBoxDevices.TabIndex = 8;
+            this.cmbBoxDevices.Text = "Выбор камеры";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 446);
+            this.Controls.Add(this.cmbBoxDevices);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSaveToFile);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.textQr);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "QRCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.TextBox textQr;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnSaveToFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbBoxDevices;
+    }
+}
+
---
+++ QRCodeApplication/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using ZXing;
+using ZXing.QrCode;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace QRCodeApplication
+{
+    public partial class Form1 : Form
+    {
+        private QrCodeEncodingOptions options;
+        private bool fromCamera = false;
+        private FilterInfoCollection devices;
+        private VideoCaptureDevice video;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250
+            };
+
+            devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in devices)
+            {
+                cmbBoxDevices.Items.Add(device.Name);
+            }
+
+            cmbBoxDevices.SelectedIndex = 0;
+            video = new VideoCaptureDevice();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var qr = new BarcodeWriter();
+            qr.Options = options;
+            qr.Format = ZXing.BarcodeFormat.QR_CODE;
+            var text = this.textQr.Text.Trim();
+            if (text.Length > 0)
+            {
+                var result = new Bitmap(qr.Write(text));
+                image.Image = result;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.image.Image == null)
+            {
+                return;
+            }
+
+            try
+            {
+                Bitmap bitmap = new Bitmap(this.image.Image);
+                BarcodeReader reader = new BarcodeReader { AutoRotate = true };
+                Result result = reader.Decode(bitmap);
+                if (result != null)
+                {
+                    string decoded = result.ToString().Trim();
+                    this.textQr.Text = decoded;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Открыть QR код из файла";
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            openFileDialog.ShowHelp = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image.Image = new Bitmap(openFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image" + ex.Message);
+                }
+            }
+        }
+
+        private void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            if (image.Image != null) 
+            {
+                saveFileDialog.Title = "Сохранить картинку как...";
+                saveFileDialog.OverwritePrompt = true;
+                saveFileDialog.CheckPathExists = true;
+                saveFileDialog.Filter = "Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                saveFileDialog.ShowHelp = true;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        image.Image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fromCamera)
+            {
+                button1.Text = "Запустить камеру";
+                video.NewFrame -= Video_NewFrame;
+                Graphics g = image.CreateGraphics();
+                g.Clear(Color.White);
+                video.Stop();
+                this.btnGenerate.Enabled = true;
+                this.btnLoadFile.Enabled = true;
+                this.btnSaveToFile.Enabled = true;
+            }
+            else
+            {
+                button1.Text = "Остановить камеру";
+                video = new VideoCaptureDevice(devices[cmbBoxDevices.SelectedIndex].MonikerString);
+                video.NewFrame += new NewFrameEventHandler(Video_NewFrame);
+                video.Start();
+                this.btnGenerate.Enabled = false;
+                this.btnLoadFile.Enabled = false;
+                this.btnSaveToFile.Enabled = false;
+
+            }
+
+            fromCamera = !fromCamera;
+        }
+
+        private void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            image.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void btnGetImage_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
